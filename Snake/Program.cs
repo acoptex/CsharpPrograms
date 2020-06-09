75732a865850f4f29bf9591679cc35cec0795757
ACuchriajev <info.acoptex@gmail.com>
2020-06-09 12:41:22 +0300

    modified Snake Lesson 2

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -6,8 +6,15 @@ namespace Snake
     {
         static void Main()
         {
-            Console.WriteLine("Hello world");
+            Draw(1, 3, '*');
+            Draw(4, 5, '#');
+
             Console.ReadKey();
+        }
+        static void Draw(int x,int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(sym);
         }
     }
 }
