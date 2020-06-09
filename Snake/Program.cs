dcb05fcbb26ae33ba3fba2435fa1db2f96338e6f
ACuchriajev <info.acoptex@gmail.com>
2020-06-09 21:26:59 +0300

    homework Lesson6

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -7,26 +7,30 @@ namespace Snake
     {
         static void Main()
         {
-            Point p1 = new Point(1,3,'*');
-            Point p2 = new Point(4,5,'#');
+            //creating four objects
+            Point point1 = new Point(2, 3, '*');
+            Point point2 = new Point(4, 9, '#');
+            Point point3 = new Point(1, 4, '%');
+            Point point4 = new Point(6, 7, '?');
 
+            //creating list with four objects
+            List<Point> pointsList = new List<Point>();
+            pointsList.Add(point1);
+            pointsList.Add(point2);
+            pointsList.Add(point3);
+            pointsList.Add(point4);
 
-
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-            //numList.RemoveAt(0);
-            foreach (int i in numList)
+            //created list of symbols
+            List<char> symbolsList = new List<char>();
+            symbolsList.Add('*');
+            symbolsList.Add('#');
+            symbolsList.Add('*');
+            symbolsList.Add('#');
+            
+            foreach (char i in symbolsList)
             {
                 Console.WriteLine(i);
-            }
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
+            }            
             
             Console.ReadKey();
         }
