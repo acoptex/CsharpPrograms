88c76a13c1c1f4486c8bbb45ee2beb7c21e89546
ACuchriajev <info.acoptex@gmail.com>
2020-06-10 12:12:59 +0300

    modified Snake Lesson 7 with Homework

---
+++ Snake/HorizontalLine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.AccessControl;
+
+namespace Snake
+{
+    
+    class HorizontalLine
+    {
+        List<Point> pointsList;
+        public HorizontalLine(int xLeft, int xRight, int y, char sym)
+        {
+
+            pointsList = new List<Point>();
+            for (int x=xLeft; x <= xRight; x++)
+            {
+                Point p = new Point(x, y, sym);
+                pointsList.Add(p);               
+            }
+            
+
+        }
+        public void Draw()
+        {
+            foreach (Point p in pointsList)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+}
+}
